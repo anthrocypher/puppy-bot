c8d61b6a3a9021593e1c59938868552ac500fe6a
Ana Hevesi <ana@nodejitsu.com>
2013-04-11 09:51:24 -0400

    [formatting] Changed from tabs to spaces

--- puppy-bot.js
+++ puppy-bot.js
@@ -1,22 +1,22 @@
 var ircb = require('ircb'),
-		fs = require('fs');
-		
+    fs = require('fs');
+    
 var irc = ircb({
-		host: 'irc.freenode.net',
-		secure: true,
-		nick: 'omgpuppies',
-		username: 'omgpuppies',
-		realName: 'omgpuppies',
-		channels: ['#nodebombrange'],
+    host: 'irc.freenode.net',
+    secure: true,
+    nick: 'omgpuppies',
+    username: 'omgpuppies',
+    realName: 'omgpuppies',
+    channels: ['#nodebombrange'],
 }, function () {
-		console.log('Connected');
-		console.log('MOTD:\n');
-		console.log(irc.motd);
+    console.log('Connected');
+    console.log('MOTD:\n');
+    console.log(irc.motd);
 });
 
 irc.join('#nodebombrange', function (err) {
-	if (err) throw err;
-	console.log('Joined #nodebombrange');
+  if (err) throw err;
+  console.log('Joined #nodebombrange');
+  irc.say('#nodebombrange', 'Are u rdy for some puppiez?');
 });
 
-irc.say('#nodebombrange', 'Are u rdy for some puppiez?');
