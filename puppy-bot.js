1304b0864fd0b9899464b73d25b55c29e47a0eea
Ana Hevesi <ana@nodejitsu.com>
2013-04-12 16:49:41 -0400

    [api] Add random puppy links generator and JSON config file.

--- puppy-bot.js
+++ puppy-bot.js
@@ -1,5 +1,12 @@
 var ircb = require('ircb'),
     fs = require('fs');
+
+var puppies = require('./puppies.json');
+
+function deliverPuppies() {
+	var puppy = puppies[Math.floor(Math.random() * puppies.length)];
+	irc.say('#nodebombrange', puppy);
+} 
     
 var irc = ircb({
     host: 'irc.freenode.net',
@@ -7,12 +14,13 @@ var irc = ircb({
     nick: 'omgpuppies',
     username: 'omgpuppies',
     realName: 'omgpuppies',
-    channels: ['#nodebombrange'],
+    channels: ['#nodebombrange']
 }, function () {
     console.log('Connected');
     console.log('MOTD:\n');
     console.log(irc.motd);
 		irc.say('#nodebombrange', 'Are u rdy for some puppiez?');
+		setInterval(deliverPuppies, 5000);
 });
 
 
